0c2f2b0dc75835c897c0ee34cec4db327e0e9882
Gurpreet S Rehian <gurpreet.rehian@sherwin.com>
2023-02-21 16:54:44 -0500

    custom deploy action

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,11 +1,19 @@
 const core = require("@actions/core")
-const github = require("@actions/github")
+//const github = require("@actions/github")
 const exec = require("@actions/exec")
 
-
-
 function run(){
- core.notice('hello from my custom java script action');
+    
+    // Input values
+    const bucket = core.getInput('bucket',{required:true});
+    const bucketRegion = core.getInput('bucket-region',{required:true});
+    const distFolder = core.getInput('dist-folder',{required:true});
+
+    //upload files
+    const s3Uri= `s3://${bucket}`;
+    exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
+
+    core.notice('hello from my custom java script action');
 }
 
 run();
