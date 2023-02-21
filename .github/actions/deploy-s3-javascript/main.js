7f04e75ad14780eafd028cfebaecd4e9bff9b2d9
Gurpreet S Rehian <gurpreet.rehian@sherwin.com>
2023-02-21 17:16:05 -0500

    custom JS code

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -4,6 +4,9 @@ const exec = require("@actions/exec")
 
 function run(){
     
+    core.notice('***Custom Java Script Action ****');
+    
+
     // Input values
     const bucket = core.getInput('bucket',{required:true});
     const bucketRegion = core.getInput('bucket-region',{required:true});
@@ -13,7 +16,10 @@ function run(){
     const s3Uri= `s3://${bucket}`;
     exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
 
-    core.notice('hello from my custom java script action');
-}
+    const webSiteUrl= `https://${{bucket}}.s3-website-.${bucketRegion}.amazonaws.com`;
+   
+    core.setOutput('website-url',webSiteUrl);
+
+}  
 
 run();
